3b46bef2c6a3e68875398bdb70d5ac13c07e2348
Ryan Ly <rly@lbl.gov>
2025-08-04 22:46:52 -0400

    Update version to 0.2.2 (#16)

--- src/spec/create_extension_spec.py
+++ src/spec/create_extension_spec.py
@@ -15,7 +15,7 @@ def main():
     # these arguments were auto-generated from your cookiecutter inputs
     ns_builder = NWBNamespaceBuilder(
         name="""ndx-franklab-novela""",
-        version="""0.2.1""",
+        version="""0.2.2""",
         doc="""NWB extension to store additional metadata and data types for Loren Frank's Lab""",
         author=[
             "NovelaNeurotechnologies",
